c73f021ec15b918d34eb4a6057d67ad62579372c
guodong <guodong915@163.com>
2016-09-17 20:11:22 +0800

    first commit

---
+++ app/src/main/java/com/foodorder/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.foodorder.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.foodorder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
