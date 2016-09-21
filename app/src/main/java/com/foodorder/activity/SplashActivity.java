8d1dc1022ed00f673994a10b84d7b5b428bb3e3f
guodong <guodong915@163.com>
2016-09-21 17:24:13 +0800

    add splash

--- app/src/main/java/com/foodorder/activity/SplashActivity.java
+++ app/src/main/java/com/foodorder/activity/SplashActivity.java
@@ -1,15 +1,61 @@
 package com.foodorder.activity;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
 
 import com.foodorder.R;
+import com.foodorder.base.BaseActivity;
+import com.foodorder.util.StringUtil;
 
-public class SplashActivity extends AppCompatActivity {
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class SplashActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+    protected int getLayoutId() {
+        return R.layout.activity_splash;
     }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+        Observable.create(new Observable.OnSubscribe<Object>() {
+            @Override
+            public void call(Subscriber<? super Object> subscriber) {
+                String menu_json = StringUtil.getJson(SplashActivity.this, "menu.json");
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<Object>() {
+            @Override
+            public void onCompleted() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Object o) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+
 }
---
+++ app/src/main/java/com/foodorder/logic/UserManager.java
@@ -0,0 +1,33 @@
+package com.foodorder.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by guodong on 2016/9/19 17:12.
+ */
+public class UserManager {
+
+    private volatile static UserManager instance;
+
+    private List<String> userList;
+
+    private UserManager() {
+        userList = new ArrayList<>();
+    }
+
+    public static UserManager getInstance() {
+        if (instance == null) {
+            synchronized (UserManager.class) {
+                if (instance == null) {
+                    instance = new UserManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public List<String> getUserList() {
+        return userList;
+    }
+}
